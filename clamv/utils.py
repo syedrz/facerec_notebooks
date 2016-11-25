a64f0a9cba530e2208ad466fe1b3f7b8478e0394
Piotr Brzozowski <git@brzozowski.uk>
2016-11-25 17:31:58 +0000

    Timing functions

--- clamv/utils.py
+++ clamv/utils.py
@@ -2,6 +2,7 @@ from sklearn.datasets import fetch_lfw_people
 from sklearn.model_selection import cross_val_score
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 
 def get_images(color=False):
@@ -17,9 +18,15 @@ def get_images(color=False):
     return X, y
 
 def test(model, X, y, f='accuracy', k=10):
+    start = time.time()
+    print('start: ', start)
+
     np.random.seed(42)
 
     results = cross_val_score(model, X, y, scoring=f, cv=k)
+
+    end = time.time()
+    print('end', end, 'duration', end - start)
 
     return np.mean(results), np.std(results)
 
