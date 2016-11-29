62e569e5afe3c429f41dec48f93221bb7b9b0d9d
Piotr Brzozowski <git@brzozowski.uk>
2016-11-29 17:52:41 +0000

    Save results

--- clamv/utils.py
+++ clamv/utils.py
@@ -1,12 +1,13 @@
 from sklearn.datasets import fetch_lfw_people
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.base import clone
 import numpy as np
 import matplotlib.pyplot as plt
 import time
 
 
-def get_images(color=False):
-    d = fetch_lfw_people(color=color, min_faces_per_person=70, resize=1)
+def get_images(color=False, min_faces_per_person=70):
+    d = fetch_lfw_people(color=color, min_faces_per_person=min_faces_per_person, resize=1)
 
     if color:
         X = d.images
@@ -17,18 +18,24 @@ def get_images(color=False):
 
     return X, y
 
-def test(model, X, y, f='accuracy', k=10):
+def test(model, X, y, filename, k=10):
     start = time.time()
     print('start: ', start)
 
-    np.random.seed(42)
+    results = []
 
-    results = cross_val_score(model, X, y, scoring=f, cv=k)
+    skf = StratifiedKFold(n_splits=k, random_state=42)
+
+    for train, test in skf.split(X, y):
+        m = clone(model)
+        m.fit(X[train], y[train])
+        y_pred = m.predict(X[test])
+        results.append((y[test], y_pred))
 
     end = time.time()
     print('end', end, 'duration', end - start)
 
-    return np.mean(results), np.std(results)
+    np.save(filename, results)
 
 def show(img, *args, **kwargs):
     plt.imshow(img.astype(np.uint8), *args, **kwargs)
