cdae152b03aff8b8b892a09d5676612f798b8d0f
Piotr Brzozowski <git@brzozowski.uk>
2016-11-30 16:15:43 +0000

    Fixed Eigenfaces to use SVM not kNN. Added basic color

--- clamv/__init__.py
+++ clamv/__init__.py
@@ -1,3 +1,4 @@
 from clamv.utils import get_images, test, show, score, up_to_per_person
 from clamv.eigenfaces import TransformerClassifier
 from clamv.opencv import OpenCVClassifier
+from clamv.color import ColorClassifier
