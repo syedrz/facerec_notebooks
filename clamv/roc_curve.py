f82898bdac39426c89310cdcbc0688dfa8685164
Thomas Allsop <thomas.allsop11@gmail.com>
2016-11-30 00:20:57 +0000

    ROC curve function implemented

---
+++ clamv/roc_curve.py
@@ -0,0 +1,30 @@
+import numpy as np
+import scipy
+
+from sklearn.metrics import roc_curve
+
+def construct_multiclass_roc_curve(y_true, y_pred, num_classes):
+	roc_classes = []
+	for i in range(num_classes):
+		fpr, tpr, thresholds = roc_curve(y_true, y_pred, pos_label=i)
+
+		roc_classes.append([fpr, tpr])
+
+	# Now take all fpr values
+	all_fpr = []
+	for p in roc_classes:
+		all_fpr = np.concatenate(p[0])
+
+	all_fpr = np.unique(all_fpr)
+
+	mean_tpr = np.zeros_live(all_fpr)
+
+	for i in range(num_classes):
+		mean_tpr = interp(all_fpr, roc_classes[i][0], roc_classes[i][1])
+
+	mean_tpr /= num_classes
+
+	final_fpr = all_fpr
+	final_tpr = mean_tpr
+
+	return final_fpr, final_tpr
